fb548c7d0e1878c63ca894bebabbcf1d5c975247
Daniel Kavassy <kavassy@kavassy.lon.corp.google.com>
2017-08-03 17:13:32 +0100

    made struct private

--- client/monitoring.go
+++ client/monitoring.go
@@ -19,7 +19,7 @@ var targetsFlag = flag.String("targets", "localhost:5000,localhost:4000,localhos
 var metricsFlag = flag.String("metrics", "requests,errors", "A comma-separeted list of metrics to collect")
 var intervalFlag = flag.Int64("interval", 60, "Scraping interval in seconds")
 
-type Monitoring struct {
+type monitoring struct {
 	values      map[int64]map[string]int64
 	rates       map[string]float64
 	logicalTime int64 // Handle overflow
@@ -29,7 +29,7 @@ type Monitoring struct {
 	mux         sync.Mutex
 }
 
-func (m *Monitoring) scrapeTarget(target, metric string, ch chan int64) {
+func (m *monitoring) scrapeTarget(target, metric string, ch chan int64) {
 	url := fmt.Sprintf("http://%s/metrics/%s", target, metric)
 	resp, err := http.Get(url)
 	if err != nil {
@@ -50,7 +50,7 @@ func (m *Monitoring) scrapeTarget(target, metric string, ch chan int64) {
 }
 
 // Start starts the scraping loop
-func (m *Monitoring) Start() {
+func (m *monitoring) Start() {
 	for {
 		m.values[m.logicalTime] = make(map[string]int64)
 		for _, metric := range m.metrics {
@@ -74,22 +74,22 @@ func (m *Monitoring) Start() {
 	}
 }
 
-func (m *Monitoring) updateRate(metric string, currentVal int64) {
+func (m *monitoring) updateRate(metric string, currentVal int64) {
 	m.mux.Lock()
 	defer m.mux.Unlock()
 	m.rates[metric] = float64(currentVal-m.values[m.logicalTime-1][metric]) / float64(m.interval)
 }
 
 // GetRate gets per second rate for specified metrics
-func (m *Monitoring) GetRate(metric string) float64 {
+func (m *monitoring) GetRate(metric string) float64 {
 	m.mux.Lock()
 	defer m.mux.Unlock()
 	return m.rates[metric]
 }
 
 // NewMonitoring creates a new monitoring object from a list of targets, metrics and scraping interval
-func NewMonitoring(targets, metrics []string, scrapingInterval int64) *Monitoring {
-	m := new(Monitoring)
+func NewMonitoring(targets, metrics []string, scrapingInterval int64) *monitoring {
+	m := new(monitoring)
 	m.logicalTime = 0
 	m.values = make(map[int64]map[string]int64)
 	m.rates = make(map[string]float64)
@@ -99,7 +99,7 @@ func NewMonitoring(targets, metrics []string, scrapingInterval int64) *Monitorin
 	return m
 }
 
-func startServer(m *Monitoring) {
+func startServer(m *monitoring) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		for _, metric := range m.metrics {
 			fmt.Fprintf(w, "%s/s: %.1f\n", metric, m.GetRate(metric))
